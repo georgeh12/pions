e046b9b7f6182d0b706e3db4b43678c636641ae3
georgehardigg@gmail.com <georgehardigg@gmail.com@50286e30-ab85-11de-9787-55e617d4dd53>
2009-11-01 23:21:16 +0000

    updated calendars and added an iterator for alerts

---
+++ trunk/PIONS/src/pions/controller/AlertIterator.java
@@ -0,0 +1,50 @@
+
+package pions.controller;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Observer;
+import pions.model.Alert;
+
+/**
+ * Iterator design implementation.
+ * @author George
+ */
+public class AlertIterator implements Iterator {
+    private Iterator<Alert> iter;
+    private Alert alert = null;
+    private Observer observer;
+
+    public AlertIterator(Iterator<Alert> iter, Observer observer) {
+        this.iter = iter;
+        this.observer = observer;
+    }
+
+    public boolean hasNext() {
+        return iter.hasNext();
+    }
+
+    /**
+     * Each alert returns 3 strings:
+     * The first is the toString() method of the EmailAddress
+     * The second is the toString() method of the AlertType
+     * The third is the toString() method of the alert object
+     * @return
+     */
+    public ArrayList<String> next() {
+        ArrayList<String> alerts = new ArrayList<String>();
+
+        alert = iter.next();
+        alert.addObserver(observer);
+
+        alerts.add(alert.getAddress().toString());
+        alerts.add(alert.getType().toString());
+        alerts.add(alert.get().toString());
+
+        return alerts;
+    }
+
+    public void remove() {
+        throw new UnsupportedOperationException("Not supported.");
+    }
+}
