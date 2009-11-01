17706b908cbad2243caf4ad996888ffefaebdedc
georgehardigg@gmail.com <georgehardigg@gmail.com@50286e30-ab85-11de-9787-55e617d4dd53>
2009-11-01 23:21:16 +0000

    updated calendars and added an iterator for alerts

--- PIONS/src/pions/controller/Gmail.java
+++ PIONS/src/pions/controller/Gmail.java
@@ -4,15 +4,18 @@ package pions.controller;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Observer;
 import javax.mail.MessagingException;
 import javax.mail.NoSuchProviderException;
 import javax.mail.internet.AddressException;
 import pions.model.ContactInfo.EmailAddress;
 import pions.model.EmployeeSingleton;
+import pions.model.ModelException.AlertClassException;
 import pions.model.ModelException.NotLoggedInException;
 import pions.model.Alert;
 import pions.model.Alert.AlertType;
+import pions.model.Employee;
 import pions.model.swapshift.SwapShift;
 
 /**
@@ -20,6 +23,30 @@ import pions.model.swapshift.SwapShift;
  * @author George
  */
 public class Gmail implements Serializable {
+    public Iterator getActiveAlertIterator(Observer observer) {
+        try {
+            return new AlertIterator(EmployeeSingleton.getInstance().getGmail().getActiveAlerts(),
+                    observer);
+        } catch (NotLoggedInException e) {
+            e.printStackTrace();
+        } finally {
+            //might cause cascading View exception
+            return null;
+        }
+    }
+
+    public Iterator getSavedAlertIterator(Observer observer) {
+        try {
+            return new AlertIterator(EmployeeSingleton.getInstance().getGmail().getSavedAlerts(),
+                    observer);
+        } catch (NotLoggedInException e) {
+            e.printStackTrace();
+        } finally {
+            //might cause cascading View exception
+            return null;
+        }
+    }
+
     public void setGmail(Observer observer, String gmail_username, String gmail_password){
         try {
             pions.model.Gmail gmail = EmployeeSingleton.getInstance().getGmail();
@@ -34,14 +61,15 @@ public class Gmail implements Serializable {
 
     public ArrayList<String> parseAlerts() {
         ArrayList<String> exceptions = new ArrayList<String>();
-
+        
         try {
             pions.model.Gmail gmail = EmployeeSingleton.getInstance().getGmail();
 
             for(Exception exception: gmail.parseAlerts()){
-                exceptions.add(exception.toString());
+                exceptions.add(exception.getCause().getMessage());
+                exceptions.add(exception.getMessage());
             }
-            
+
             gmail.notifyObservers();
         } catch (NoSuchProviderException e) {
             e.printStackTrace();
@@ -111,31 +139,61 @@ public class Gmail implements Serializable {
         return selected;
     }
 
+    public void sendNewManager(EmployeeSingleton manager){
+        try {
+            sendAlert(EmployeeSingleton.getInstance().getManagerGmails(),
+                    new Alert((Employee)manager, AlertType.AddManager));
+        } catch (NotLoggedInException e) {
+            e.printStackTrace();
+        } catch (AlertClassException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendNewSubordinate(EmployeeSingleton subordinate){
+        try {
+            sendAlert(EmployeeSingleton.getInstance().getSubordinateGmails(),
+                    new Alert((Employee)subordinate, AlertType.AddSubordinate));
+        } catch (NotLoggedInException e) {
+            e.printStackTrace();
+        } catch (AlertClassException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void sendNewWorkSchedule(boolean[] indices){
         try {
             sendWorkSchedule(getSelectedEmails(EmployeeSingleton.getInstance().getSubordinateGmails(), indices),
                     AlertType.NewWorkSchedule);
         } catch (NotLoggedInException e) {
             e.printStackTrace();
+        } catch (AlertClassException e) {
+            e.printStackTrace();
         }
     }
 
     public void sendUpdatedWorkSchedule(boolean[] indices){
         try {
-            sendWorkSchedule(getSelectedEmails(EmployeeSingleton.getInstance().getSubordinateGmails(), indices),
-                    AlertType.UpdatedWorkSchedule);
+            sendWorkSchedule(getSelectedEmails(EmployeeSingleton.getInstance().getSubordinateGmails(), indices), AlertType.UpdatedWorkSchedule);
         } catch (NotLoggedInException e) {
+            e.printStackTrace();
+        } catch (AlertClassException e) {
             e.printStackTrace();
         }
     }
 
     private void sendWorkSchedule(ArrayList<EmailAddress> gmail_addresses, AlertType alert_type)
-            throws NotLoggedInException {
-        sendAlert(gmail_addresses, new Alert(EmployeeSingleton.getInstance().getWorkSchedule(), alert_type));
+            throws NotLoggedInException, AlertClassException {
+        sendAlert(gmail_addresses,
+                new Alert(EmployeeSingleton.getInstance().getCalendars().getWorkSchedule(), alert_type));
     }
 
     public void sendSwapShift(SwapShift swap_shift) throws NotLoggedInException{
-        sendAlert(swap_shift.getRecipients(), new Alert(swap_shift, AlertType.SwapShift));
+        try {
+            sendAlert(swap_shift.getRecipients(), new Alert(swap_shift, AlertType.SwapShift));
+        } catch (AlertClassException e) {
+            e.printStackTrace();
+        }
     }
 
     private void sendAlert(ArrayList<EmailAddress> gmail_addresses, Alert alert)
