fb6b4301db2cbefcd709726c6c3d22740c537e68
georgehardigg@gmail.com <georgehardigg@gmail.com@50286e30-ab85-11de-9787-55e617d4dd53>
2009-11-08 07:54:31 +0000

    partially set up GUI

---
+++ PIONS/src/pions/PIONS.java
@@ -0,0 +1,38 @@
+
+package pions;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+import pions.view.PIONSView;
+
+/**
+ * The main class of the application.
+ * @author George
+ */
+public class PIONS extends SingleFrameApplication {
+
+    /**
+     * At startup create and show the main frame of the application.
+     */
+    @Override
+    protected void startup() {
+        PIONSView main_frame = new PIONSView();
+        setMainFrame(main_frame);
+        show(main_frame);
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of PIONS
+     */
+    public static PIONS getApplication() {
+        return Application.getInstance(PIONS.class);
+    }
+
+    /**
+     * Main method launching the application.
+     */
+    public static void main(String[] args) {
+        getApplication().startup();
+    }
+}
--- PIONS/src/pions/view/NewAlerts.java
+++ PIONS/src/pions/view/NewAlerts.java
@@ -1,20 +1,16 @@
 
-/*
- * NewEmployee.java
- *
- * @author George
- */
-
 package pions.view;
+
+import javax.swing.JPanel;
 
 /**
  *
  * @author George
  */
-public class NewEmployee extends javax.swing.JFrame {
+public class NewAlerts extends JPanel {
 
-    /** Creates new form NewEmployee */
-    public NewEmployee() {
+    /** Creates new form NewAlerts */
+    public NewAlerts() {
         initComponents();
     }
 
@@ -27,11 +23,10 @@ public class NewEmployee extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setName("Form"); // NOI18N
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 400, Short.MAX_VALUE)
@@ -40,20 +35,8 @@ public class NewEmployee extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 300, Short.MAX_VALUE)
         );
-
-        pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    /**
-    * @param args the command line arguments
-    */
-    public static void main(String args[]) {
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new NewEmployee().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     // End of variables declaration//GEN-END:variables
