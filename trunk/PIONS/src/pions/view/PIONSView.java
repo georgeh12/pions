6bfc810618137e59b015957ea6cacf5c847fd930
georgehardigg@gmail.com <georgehardigg@gmail.com@50286e30-ab85-11de-9787-55e617d4dd53>
2009-11-11 07:06:31 +0000

    started working on the menu listeners. added an exit listener to confirm exits and save information if the user is logged in.

--- trunk/PIONS/src/pions/view/PIONSView.java
+++ trunk/PIONS/src/pions/view/PIONSView.java
@@ -3,8 +3,11 @@ package pions.view;
 
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.EventObject;
 import java.util.Observable;
 import java.util.Observer;
 import javax.swing.JFrame;
@@ -13,6 +16,7 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import org.jdesktop.application.Application.ExitListener;
 import pions.PIONS;
 import pions.controller.Employees;
 
@@ -41,7 +45,20 @@ public class PIONSView implements Observer {
 
         main_frame.setPreferredSize(new Dimension(600,400));
         main_frame.getContentPane().setLayout(new GridLayout(1,2));
-        main_frame.addWindowListener(new PIONSWindowListener());
+
+        //TODO implement PIONSPanel interface to give panels a window closing function.
+        PIONS.getApplication().addExitListener(new ExitListener(){
+            public boolean canExit(EventObject evt) {
+                return JOptionPane.YES_OPTION ==
+                        JOptionPane.showConfirmDialog(getFrame(),
+                        "Are you sure you want to exit?",
+                        "Confirm Exit",
+                        JOptionPane.YES_NO_OPTION);
+            }
+            public void willExit(EventObject evt) {
+                if(Employees.isLoggedIn()) Employees.logout();
+            }
+        });
 
         initMenu();
 
@@ -52,21 +69,86 @@ public class PIONSView implements Observer {
         PIONS.getApplication().show(main_frame);
     }
 
+    /**
+     * Initializes the menu bar and adds an ActionListener to each menu item.
+     */
     private void initMenu(){
         JMenuBar menu = new JMenuBar();
+
+        //File Menu
         JMenu file = new JMenu("File");
+
         menu_quit = file.add(new JMenuItem("Quit"));
+        menu_quit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                menu_quitActionPerformed(evt);
+            }
+        });
+
+        //Alert Menu
         JMenu alerts = new JMenu("Alert");
+
         menu_checkalerts = alerts.add(new JMenuItem("Check For New Alerts"));
+        menu_checkalerts.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                menu_checkalertsActionPerformed(evt);
+            }
+        });
+
         menu_activealerts = alerts.add(new JMenuItem("Active Alerts"));
+        menu_activealerts.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                menu_activealertsActionPerformed(evt);
+            }
+        });
+
         menu_savedalerts = alerts.add(new JMenuItem("Saved Alerts"));
+        menu_savedalerts.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                menu_savedalertsActionPerformed(evt);
+            }
+        });
+
+        //Window Menu
         JMenu window = new JMenu("Window");
+
         menu_login = window.add(new JMenuItem("Login"));
+        menu_login.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                menu_loginActionPerformed(evt);
+            }
+        });
+
         menu_managers = window.add(new JMenuItem("Managers"));
+        menu_managers.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                menu_managersActionPerformed(evt);
+            }
+        });
+
         menu_subordinates = window.add(new JMenuItem("Subordinates"));
+        menu_subordinates.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                menu_subordinatesActionPerformed(evt);
+            }
+        });
+
         menu_calendars = window.add(new JMenuItem("Calendars"));
+        menu_calendars.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                menu_calendarsActionPerformed(evt);
+            }
+        });
+
+        //Help Menu
         JMenu help = new JMenu("Help");
         menu_aboutus = help.add(new JMenuItem("About Us"));
+        menu_aboutus.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                menu_aboutusActionPerformed(evt);
+            }
+        });
+        
         menu.add(file);
         menu.add(alerts);
         menu.add(window);
@@ -134,14 +216,40 @@ public class PIONSView implements Observer {
         JOptionPane.showMessageDialog(getFrame(), arg, "System Message", JOptionPane.INFORMATION_MESSAGE);
     }
 
-    //TODO implement PIONSPanel interface to give panels a window closing function.
-    /**
-     * This listener will logout the employee before the window closes.
-     */
-    private class PIONSWindowListener extends WindowAdapter {
-        @Override
-        public void windowClosing(WindowEvent e){
-            if(Employees.isLoggedIn()) Employees.logout();
-        }
+    private void menu_quitActionPerformed(ActionEvent evt){
+        PIONS.getApplication().exit(evt);
+    }
+
+    //TODO implement the following methods
+    private void menu_checkalertsActionPerformed(ActionEvent evt){
+
+    }
+
+    private void menu_activealertsActionPerformed(ActionEvent evt){
+
+    }
+
+    private void menu_savedalertsActionPerformed(ActionEvent evt){
+
+    }
+
+    private void menu_loginActionPerformed(ActionEvent evt){
+
+    }
+
+    private void menu_managersActionPerformed(ActionEvent evt){
+
+    }
+
+    private void menu_subordinatesActionPerformed(ActionEvent evt){
+
+    }
+
+    private void menu_calendarsActionPerformed(ActionEvent evt){
+
+    }
+
+    private void menu_aboutusActionPerformed(ActionEvent evt){
+
     }
 }
