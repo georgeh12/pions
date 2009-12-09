c3dc0e8dffcfbeb3e5de0ebccfaf664483e322f7
georgehardigg@gmail.com <georgehardigg@gmail.com@50286e30-ab85-11de-9787-55e617d4dd53>
2009-12-09 12:18:36 +0000

    demo files uploaded. final version

--- trunk/PIONS/src/pions/model/dropshift/DecoratorIgnored.java
+++ trunk/PIONS/src/pions/model/dropshift/DecoratorIgnored.java
@@ -15,6 +15,11 @@ class DecoratorIgnored extends AbstractDecorator implements Serializable {
     }
 
     @Override
+    protected ArrayList<EmailAddress> getRecipients() {
+        return new ArrayList<EmailAddress>();
+    }
+
+    @Override
     protected boolean isAccepted() {
         return false;
     }
@@ -27,10 +32,5 @@ class DecoratorIgnored extends AbstractDecorator implements Serializable {
     @Override
     protected boolean isIgnored() {
         return true;
-    }
-
-    @Override
-    protected ArrayList<EmailAddress> getRecipients() {
-        return new ArrayList<EmailAddress>();
     }
 }
