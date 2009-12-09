ef8f7e3ac401a5dc903e418275786f3dfcfd7d09
georgehardigg@gmail.com <georgehardigg@gmail.com@50286e30-ab85-11de-9787-55e617d4dd53>
2009-12-09 12:18:36 +0000

    demo files uploaded. final version

--- PIONS/src/pions/model/dropshift/DecoratorRejected.java
+++ PIONS/src/pions/model/dropshift/DecoratorRejected.java
@@ -13,21 +13,6 @@ class DecoratorRejected extends AbstractDecorator implements Serializable {
     protected DecoratorRejected(AbstractState drop_shift_state){
         super(drop_shift_state);
     }
-    
-    @Override
-    protected void accepted() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    protected void rejected() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    protected void ignored() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
 
     @Override
     protected ArrayList<EmailAddress> getRecipients() {
